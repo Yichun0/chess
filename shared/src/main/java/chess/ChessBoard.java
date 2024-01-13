f11e6b481987ba07644ec43c3b257596127d4e4e
Abby Chen <abbyc02@byu.edu>
2024-01-12 23:18:52 -0700

    01/12/24

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,8 +8,8 @@ package chess;
  */
 public class ChessBoard {
     int i = 0;
+    private ChessPiece[][] borad = new ChessPiece[8][8];
     public ChessBoard() {
-        
     }
 
     /**
@@ -30,7 +30,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return borad[position.getRow()][position.getColumn()];
     }
 
     /**
